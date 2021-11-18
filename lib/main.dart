7de7141f251775372f672adee4da659793cc1bdc
firdauskotp <firdauskotp@gmail.com>
2021-11-18 20:28:40 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+//      title: "Register",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            color: Color(0xFF37474F),
+          )),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget{
+//  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title = "HOME PAGE";
+
+
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: ExactAssetImage("images/homewallpaper.jpg"),
+                  fit: BoxFit.cover
+              )
+          ),
+
+          child: SingleChildScrollView(
+
+            child: Form(
+
+
+              child: Column(
+
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+
+                  SizedBox(
+                      height:80.0
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical:16.0, horizontal: 16.0),
+                    child: Divider(),
+                  ),
+                  // Padding(
+                  //     padding: EdgeInsets.symmetric(horizontal: 16.0, vertical:12.0),
+                  //     child: MaterialButton(
+                  //       minWidth: 1000,
+                  //       child: Text('Have an account? LOGIN HERE',
+                  //         style: TextStyle(
+                  //           color: Colors.white,
+                  //         ),),
+                  //       color: Colors.grey,
+                  //       onPressed: (){
+                  //         Navigator.of(context).push(
+                  //             MaterialPageRoute(
+                  //               builder: (context) => LoginPage(),
+                  //             )
+                  //         );
+                  //       },
+                  //     )
+                  // ),
+                  // Padding(
+                  //   padding: EdgeInsets.symmetric(horizontal: 16.0, vertical:12.0),
+                  //   child: MaterialButton(
+                  //     minWidth: 1000,
+                  //     child: Text('Create an account? REGISTER HERE',
+                  //       style: TextStyle(
+                  //         color: Colors.white,
+                  //       ),),
+                  //     color: Colors.grey,
+                  //     onPressed: (){
+                  //       Navigator.of(context).push(
+                  //           MaterialPageRoute(
+                  //             builder: (context) => RegisterPage(),
+                  //           )
+                  //       );
+                  //     },
+                  //   ),
+                  // ),
+                  // Padding(
+                  //   padding: EdgeInsets.symmetric(horizontal: 16.0, vertical:12.0),
+                  //   child: MaterialButton(
+                  //     minWidth: 1000,
+                  //     child: Text('Wish to donate? DONATE HERE',
+                  //       style: TextStyle(
+                  //         color: Colors.white,
+                  //       ),),
+                  //     color: Colors.grey,
+                  //     onPressed: (){
+                  //       Navigator.of(context).push(
+                  //           MaterialPageRoute(
+                  //             builder: (context) => MyDonationPage(),
+                  //           )
+                  //       );
+                  //     },
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+//      ),
+    );
+  }
+}
